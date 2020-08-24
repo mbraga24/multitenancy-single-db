be3dab6f6f30e039dc5d4f092abc6d6441b41c1c
github username <rdg.brg@gmail.com>
2020-08-24 15:12:54 -0400

    Finish setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_account
+
+  def require_account!
+    redirect_to root_url(subdomain: "www") if !@account.present?
+  end
+
+  def set_account
+    @account = Account.find_by(subdomain: request.subdomain)
+    # byebug
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password,
+                                      :password_confirmation, account_attributes: [:subdomain] ])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one :account
+  accepts_nested_attributes_for :account
+
+  after_initialize :set_account
+
+  private
+
+    def set_account
+      build_account unless account.present?
+    end
+end
